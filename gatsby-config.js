8d71967796eb1705020f55dfe046c27cdc036f2f
Lauren Albert <laurenalbert@Laurens-MBP.home>
2019-07-09 13:59:40 -0400

    developed tutorial part 4, but can't see what I'm supposed to see in local host, only Hello world

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {
-  /* Your site config here */
-}
+ module.exports = {
+   plugins: [
+     `gatsby-plugin-emotion`,
+     {
+       resolve: `gatsby-plugin-typography`,
+       options: {
+         pathToConfigModule: `src/utils/typography`,
+       },
+     },
+   ],
+ }
